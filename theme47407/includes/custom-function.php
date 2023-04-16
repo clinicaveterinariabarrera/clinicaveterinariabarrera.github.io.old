24344db48ccf44d49d3ceeb1534b2798335034e8
clinicaveterinariabarrera <130927977+clinicaveterinariabarrera@users.noreply.github.com>
2023-04-16 16:12:24 -0700

    uploads

---
+++ theme47407/includes/custom-function.php
@@ -0,0 +1,171 @@
+<?php
+	// Loading child theme textdomain
+	load_child_theme_textdomain( CURRENT_THEME, get_stylesheet_directory() . '/languages' );
+
+	require_once('my_script.php');
+	
+	// WP Pointers
+	add_action('admin_enqueue_scripts', 'myHelpPointers');
+	function myHelpPointers() {
+	//First we define our pointers 
+	$pointers = array(
+	   	array(
+	       'id' => 'xyz1',   // unique id for this pointer
+	       'screen' => 'options-permalink', // this is the page hook we want our pointer to show on
+	       'target' => '#submit', // the css selector for the pointer to be tied to, best to use ID's
+	       'title' => theme_locals("submit_permalink"),
+	       'content' => theme_locals("submit_permalink_desc"),
+	       'position' => array( 
+	                          'edge' => 'top', //top, bottom, left, right
+	                          'align' => 'left', //top, bottom, left, right, middle
+	                          'offset' => '0 5'
+	                          )
+	       ),
+
+	    array(
+	       'id' => 'xyz2',   // unique id for this pointer
+	       'screen' => 'themes', // this is the page hook we want our pointer to show on
+	       'target' => '#toplevel_page_options-framework', // the css selector for the pointer to be tied to, best to use ID's
+	       'title' => theme_locals("import_sample_data"),
+	       'content' => theme_locals("import_sample_data_desc"),
+	       'position' => array( 
+	                          'edge' => 'bottom', //top, bottom, left, right
+	                          'align' => 'top', //top, bottom, left, right, middle
+	                          'offset' => '0 -10'
+	                          )
+	       ),
+
+	    array(
+	       'id' => 'xyz3',   // unique id for this pointer
+	       'screen' => 'toplevel_page_options-framework', // this is the page hook we want our pointer to show on
+	       'target' => '#toplevel_page_options-framework', // the css selector for the pointer to be tied to, best to use ID's
+	       'title' => theme_locals("import_sample_data"),
+	       'content' => theme_locals("import_sample_data_desc_2"),
+	       'position' => array( 
+	                          'edge' => 'left', //top, bottom, left, right
+	                          'align' => 'top', //top, bottom, left, right, middle
+	                          'offset' => '0 18'
+	                          )
+	       )
+	    // more as needed
+	    );
+		//Now we instantiate the class and pass our pointer array to the constructor 
+		$myPointers = new WP_Help_Pointer($pointers); 
+	};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Recent Testimonials
+if (!function_exists('shortcode_recenttesti')) {
+
+	function shortcode_recenttesti($atts, $content = null) {
+		extract(shortcode_atts(array(
+				'num'           => '5',
+				'thumb'         => 'true',
+				'excerpt_count' => '30',
+				'custom_class'  => '',
+		), $atts));
+
+		// WPML filter
+		$suppress_filters = get_option('suppress_filters');
+
+		$args = array(
+				'post_type'        => 'testi',
+				'numberposts'      => $num,
+				'orderby'          => 'post_date',
+				'suppress_filters' => $suppress_filters
+			);
+		$testi = get_posts($args);
+
+		$output = '<div class="testimonials '.$custom_class.'">';
+		
+		global $post;
+		global $my_string_limit_words;
+
+		foreach ($testi as $k => $post) {
+			// Unset not translated posts
+			if ( function_exists( 'wpml_get_language_information' ) ) {
+				global $sitepress;
+
+				$check              = wpml_get_language_information( $post->ID );
+				$language_code      = substr( $check['locale'], 0, 2 );
+				if ( $language_code != $sitepress->get_current_language() ) unset( $testi[$k] );
+
+				// Post ID is different in a second language Solution
+				if ( function_exists( 'icl_object_id' ) ) $post = get_post( icl_object_id( $post->ID, 'testi', true ) );
+			}
+			setup_postdata($post);
+			$excerpt        = get_the_excerpt();
+			$testiname      = get_post_meta(get_the_ID(), 'my_testi_caption', true);
+			$testiurl       = get_post_meta(get_the_ID(), 'my_testi_url', true);
+			$testiinfo      = get_post_meta(get_the_ID(), 'my_testi_info', true);
+			$attachment_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
+			$url            = $attachment_url['0'];
+			$image          = aq_resize($url, 280, 240, true);
+
+			$output .= '<div class="testi-item">';
+			
+
+				$output .= '<small class="testi-meta">';
+				
+					if( isset($testiname) ) { 
+						$output .= '<span class="user">';
+							$output .= $testiname;
+						$output .= '</span>';
+					}
+					
+				$output .= '</small>';
+			
+				$output .= '<blockquote class="testi-item_blockquote">';
+					if ($thumb == 'true') {
+						if ( has_post_thumbnail($post->ID) ){
+							$output .= '<figure class="featured-thumbnail">';
+							$output .= '<img src="'.$image.'" alt="" />';
+							$output .= '</figure>';
+						}
+					}
+					
+					$output .= my_string_limit_words($excerpt,$excerpt_count);
+					$output .= '<a href="'.get_permalink($post->ID).'">';
+						$output .= __('read more', CURRENT_THEME);
+					$output .= '</a><div class="clear"></div>';
+
+				$output .= '</blockquote>';
+					
+			$output .= '</div>';
+
+		}
+		$output .= '</div>';
+		return $output;
+	}
+	add_shortcode('recenttesti', 'shortcode_recenttesti');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
