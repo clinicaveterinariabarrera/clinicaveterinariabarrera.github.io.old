91b9435eadc5fbf2e7b0e04b60393e3b7ac6147d
clinicaveterinariabarrera <130927977+clinicaveterinariabarrera@users.noreply.github.com>
2023-04-18 15:42:09 -0700

    sync

---
+++ js/my_script.js
@@ -0,0 +1,7 @@
+$(window).load(function(){
+
+    jQuery("ul.news li:nth-child(1)").addClass("item_1");
+    jQuery("ul.news li:nth-child(2)").addClass("item_1");
+    jQuery("ul.news li:nth-child(3)").addClass("item_3");
+
+});
