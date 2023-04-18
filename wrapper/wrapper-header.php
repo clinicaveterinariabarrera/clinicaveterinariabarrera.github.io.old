747f76116acac568d094d39d7bdec88cdee6a7e1
clinicaveterinariabarrera <130927977+clinicaveterinariabarrera@users.noreply.github.com>
2023-04-18 15:11:00 -0700

    Add files via upload

---
+++ wrapper/wrapper-header.php
@@ -0,0 +1,38 @@
+<?php /* Wrapper Name: Header */ ?>
+
+<div class="row">
+	<div class="span12" data-motopress-type="static" data-motopress-static-file="static/static-logo.php">
+		<?php get_template_part("static/static-logo"); ?>
+	</div>
+</div>
+
+<div class="row">
+	<div class="span12 hidden-phone" data-motopress-type="static" data-motopress-static-file="static/static-search.php">
+		<?php get_template_part("static/static-search"); ?>
+	</div>
+</div>
+
+<div class="row">
+	<div class="span8" data-motopress-type="static" data-motopress-static-file="static/static-nav.php">
+		<?php get_template_part("static/static-nav"); ?>
+	</div>
+	<!-- Social Links -->
+	<div class="span4 social-nets-wrapper" data-motopress-type="static" data-motopress-static-file="static/static-social-networks.php">
+		<?php get_template_part("static/static-social-networks"); ?>
+	</div>
+	<!-- /Social Links -->
+</div>
+
+<div class="row">
+	<div class="span6" data-motopress-type="static" data-motopress-static-file="static/static-logo.php">
+		<?php get_template_part("static/static-logo"); ?>
+	</div>
+	<div class="span6 hidden-phone" data-motopress-type="static" data-motopress-static-file="static/static-search.php">
+		<?php get_template_part("static/static-search"); ?>
+	</div>
+</div>
+<div class="row">
+	<div class="<?php echo cherry_get_layout_class( 'full_width_content' ); ?>" data-motopress-type="static" data-motopress-static-file="static/static-nav.php">
+		<?php get_template_part("static/static-nav"); ?>
+	</div>
+</div>
